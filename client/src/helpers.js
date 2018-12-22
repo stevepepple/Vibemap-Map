bb6971000177090f751058fd4edce9b0a2c49908
Steve Pepple <stephen.pepple@gmail.com>
2018-12-21 18:03:21 -0800

    Helper functions for Foursquare

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -1,7 +1,10 @@
-module.exports = {
+import request from 'request'
+import * as Constants from './constants.js'
+
+const helpers = {
 
     // Get HTML Position
-    getPosition:function (options) {
+    getPosition: function (options) {
         return new Promise(function (resolve, reject) {
 
             navigator.geolocation.getCurrentPosition(function (position) {
@@ -12,7 +15,70 @@ module.exports = {
         });
     },
 
-    itemExists:function (name) {
+    searchFoursquare: function (query, latlon) {
+        console.log('query and latlon', query, latlon)
+        return new Promise(function (resolve, reject) {
+
+            request({
+                url: 'https://api.foursquare.com/v2/venues/explore',
+                method: 'GET',
+                qs: {
+                    client_id: Constants.FOURSQUARE_CLIENT_ID,
+                    client_secret: Constants.FOURSQUARE_SECRET,
+                    ll: latlon,
+                    query: query,
+                    v: '20180323',
+                    limit: 1
+                }
+            }, function (err, res, body) {
+                if (err) {
+                    reject(err);
+                } else {
+                    let results = JSON.parse(body)
+                    
+                    if (results.response.groups.length == 0) {
+                        reject('No results for search.')
+                    } else {
+                        console.log('Got Foursquare place...')
+                        resolve(JSON.parse(body));
+                    }
+                }
+            });
+        });
+    },
+
+    topFoursquareResult: function(results) {
+
+        return new Promise(function (resolve, reject) {
+
+            let top = results.response.groups[0].items[0].venue;
+            top.reason = results.response.groups[0].items[0].reason;
+            //return top; 
+
+            request({
+                url: 'https://api.foursquare.com/v2/venues/' + top.id,
+                method: 'GET',
+                qs: {
+                    client_id: Constants.FOURSQUARE_CLIENT_ID,
+                    client_secret: Constants.FOURSQUARE_SECRET,
+                    v: '20180323'
+                }
+            }, function (err, res, body) {
+                if (err) {
+                    reject(err);
+                } else {
+                    let result = JSON.parse(body);
+                    resolve(result.response.venue);
+                }
+            });
+        });
+
+        
+        
+
+    },
+
+    itemExists: function (name) {
 
         return new Promise((resolve, reject) => {
             Event.find({ name: name }).limit(1)
@@ -26,5 +92,6 @@ module.exports = {
         })
 
     }
-
 }
+
+export default helpers;
