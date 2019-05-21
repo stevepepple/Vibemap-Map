d99b48f6efc84e4097e356b53c369cec0c7077ef
Steve Pepple <stephen.pepple@gmail.com>
2019-05-21 07:46:54 -0700

    Add San Jose to search

--- client/src/components/map/search.js
+++ client/src/components/map/search.js
@@ -17,6 +17,7 @@ class LocationSearchInput extends React.Component {
                 { key: 'nearby', text: 'Nearby', coords: [42, -122], value: 'nearby' },
                 { key: 'oakland', text: 'Oakland', coords: [42, -122], value: 'oakland' },
                 { key: 'sf', text: 'San Francisco', coords :[42, -122], value: 'sf' },
+                { key: 'sanjose', text: 'San Jose', coords: [37.3, -121.8], value: 'sanjose' },
                 { key: 'guadalajara', text: 'Guadalajara', coords: [42, -122], value: 'guadalajara' },
                 { key: 'vancouver', text: 'Vancouver', coords: [42, -122], value: 'vancouver' },
                 { key: 'cairo', text: 'Cairo', coords: [31.233155, 30.042124], value: 'cairo' } 
