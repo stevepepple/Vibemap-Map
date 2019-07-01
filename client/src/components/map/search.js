950ff307e4b319278c9b3c1d2e5536a5f637a562
Steve Pepple <stephen.pepple@gmail.com>
2019-07-01 10:38:31 -0700

    Add Portland and update text

--- client/src/components/map/search.js
+++ client/src/components/map/search.js
@@ -17,6 +17,7 @@ class LocationSearchInput extends React.Component {
                 { key: 'nearby', text: 'Nearby', coords: [42, -122], value: 'nearby' },
                 { key: 'oakland', text: 'Oakland', coords: [42, -122], value: 'oakland' },
                 { key: 'sf', text: 'San Francisco', coords :[42, -122], value: 'sf' },
+                { key: 'portland', text: 'Portland', coords: [45.525781, -122.672448], value: 'portland' },
                 { key: 'sanjose', text: 'San Jose', coords: [37.3, -121.8], value: 'sanjose' },
                 { key: 'guadalajara', text: 'Guadalajara', coords: [20.67657143973132,-103.34742475872281], value: 'guadalajara' },
                 { key: 'vancouver', text: 'Vancouver', coords: [42, -122], value: 'vancouver' },
@@ -97,7 +98,7 @@ class LocationSearchInput extends React.Component {
                 onSearchChange={this.handleSearch}
                 onChange={this.handleSelect}
                 options={options}
-                placeholder='Pick a City'
+                placeholder='Near by'
             />
 
             /*
