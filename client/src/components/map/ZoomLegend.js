50ffef9bba47313c796fba67992c31bd36bc88a7
Steve Pepple <stephen.pepple@gmail.com>
2019-09-26 23:09:00 -0700

    Zoom legend component

---
+++ client/src/components/map/ZoomLegend.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import * as Constants from '../../constants.js'
+
+export default class ZoomLegend extends React.Component {
+    
+    render() {
+        
+        // Rounded for display purposes 
+        let zoom_rounded = Math.round(this.props.zoom)
+        // Give a sense of scale to each zoom level; rounded to whole integer
+        let zoom_level = Constants.zoom_levels[zoom_rounded]
+        return (
+            <div id='scale'>{zoom_rounded} : {zoom_level} </div>
+        );
+    }
+}
