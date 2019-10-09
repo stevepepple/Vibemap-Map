f5a560a1420666f25bed46110be6243a7bfe6552
Steve Pepple <stephen.pepple@gmail.com>
2019-10-09 15:33:13 -0700

    Vibe component

---
+++ client/src/components/elements/vibe.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default class Vibe extends React.Component{
+
+    componentWillMount() {
+        
+        // TODO: @cory once there's a vibe attribute it will be connected to this component.
+        let feature = this.props.feature
+        
+        if (feature.properties.vibes && feature.properties.vibes.length > 0) {
+            let vibes = feature.properties.vibes
+            let vibe = vibes[vibes.length - 1]
+            this.setState(vibe)
+        } else {
+            return null
+        }
+    }
+
+    render() {
+        return (
+            <div className="vibe">
+                Chill
+            </div>
+        )
+    }
+    
+};
