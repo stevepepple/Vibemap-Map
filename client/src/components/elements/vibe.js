6f19815ea642b1aab5018474f549d1e15d2215c3
Steve Pepple <stephen.pepple@gmail.com>
2019-11-10 16:32:26 -0800

    Hide vibes

--- client/src/components/elements/vibe.js
+++ client/src/components/elements/vibe.js
@@ -19,7 +19,7 @@ export default class Vibe extends React.Component{
     render() {
         return (
             <div className="vibe">
-                Chill
+                
             </div>
         )
     }
