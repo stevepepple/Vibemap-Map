ecf360492bf757d2dc726dd0429672bb9fdeb067
Steve Pepple <stephen.pepple@gmail.com>
2019-12-08 11:59:42 -0800

    Fix place details; Score places

--- client/src/services/VibeMap.js
+++ client/src/services/VibeMap.js
@@ -1,6 +1,8 @@
 const Constants = require('../constants')
 const querystring = require('querystring')
 const moment = require('moment')
+const helpers = require('../helpers.js')
+const turf = require('@turf/turf')
 
 const ApiHeaders = new Headers({
     'Authorization': 'Token ' + Constants.SYSTEM_TOKEN
@@ -69,7 +71,6 @@ module.exports = {
             fetch(ApiUrl + "/v0.2/events/?" + query, { headers: ApiHeaders })
                 .then(data => data.json())
                 .then(res => {
-
                     clearTimeout(timeout);
                     resolve({ data: res.results.features, loading: false, timedOut: false })
                     
@@ -94,19 +95,38 @@ module.exports = {
     },
 
     // TODO: Include a way to query by time of day
-    getTopPicks: function (point, distance, activity, vibes) {
+    getPlaceDetails: function (id) {
+        return new Promise(function (resolve, reject) {
+            fetch(ApiUrl + "/v0.2/places/" + id, { headers: ApiHeaders })
+                .then(data => data.json())
+                .then(result => {
+                    clearTimeout(timeout);
+                    console.log("Got place details: ", result)
+                    resolve({ data: result, loading: false, timedOut: false })
 
-        let distanceInMeters = distance * Constants.METERS_PER_MILE
+                }, (error) => {
+                    console.log(error)
+                })
+        })
+    },
+
+    // TODO: Include a way to query by time of day
+    getPicks: function (point, distance, activity, vibes, search) {
+ 
+        // Don't allow distance to be negative.
+        let distanceInMeters = 1
+        if (distance > 0) distanceInMeters = distance * Constants.METERS_PER_MILE
         
         // TODO: Load more points at greater distances?        
         return new Promise(function (resolve, reject) {
             let params = {
                 // lat: this.state.lat,
                 // lon: this.state.lon,
-                ordering: 'aggregateScore',
+                ordering: '-aggregate_rating',
                 point: point,
                 dist: distanceInMeters,
-                // TODO: Make two calls, one for all and one for vibe or search? 
+                categories: activity,
+                search: search,
                 vibes: vibes,
                 per_page: 50
             }
@@ -149,48 +169,125 @@ module.exports = {
                 dist: distanceInMeters,
                 categories: activity,
                 search: search_term,
-                per_page: 1000
+                per_page: 500
             }
 
             if (activity) {
                 params["category"] = activity
             }
 
-            let query = querystring.stringify(params);
+            let center_point = point.split(',').map(value => parseFloat(value))
 
-            
+            let query = querystring.stringify(params);
 
             fetch(ApiUrl + "/v0.2/places/?" + query, { headers: ApiHeaders })
                 .then(data => data.json())
                 .then(res => {
+
                     clearTimeout(timeout);
-                    
-                    //console.clear()
-                    //console.log("distance: ", distanceInMeters)
-                    console.log('Received this many places: ', res.results.features.length)
-                    res.results.features.forEach(place => {
-                        if (place.properties.aggregate_rating > 2) {
-                            if (place.properties.categories == undefined || place.properties.categories.length == 0) {
-                                place.properties.categories = ["resturaunt"]
-                            }
+                    let places_scored_and_sorted = module.exports.scorePlaces(res.results.features, center_point)
 
-                            if (place.properties.aggregate_rating > 4.0) {
-                               //console.log(place.properties.name + " " + place.properties.aggregate_rating + " " + place.properties.categories)
-                            }
+                    resolve({ data: places_scored_and_sorted, loading: false, timedOut: false })
 
-                            
-                        } else {
-                            place.properties.aggregate_rating = 2
-                        }
-                        
-                    });
-                    //console.log(res.results.features);
-
-                    resolve({ data: res.results.features, loading: false, timedOut: false })
-                    
                 }, (error) => {
                     console.log(error)
                 });
-        });
-    }
+        })
+    },
+
+    scorePlaces: function(places, center_point, vibes) {
+        let max_event_score = 1
+        let max_vibe_score = 1
+        let max_aggregate_score = 1
+        let max_distance = 1
+
+        let vibe_match_bonus = 10
+
+        let place = places.map((place) => {
+            place.properties.aggregate_rating = parseFloat(place.properties.aggregate_rating)
+
+            // Give place a vibe score
+            place.properties.vibe_score = place.properties.vibes.length
+
+            // Give direct vibe matches bonus points
+            let vibe_matches = 0
+            let vibe_bonus = 0
+            if (vibes && place.properties.vibes) {
+                vibe_matches = helpers.default.matchLists(vibes, place.properties.vibes)
+            }
+
+            vibe_bonus = vibe_matches * vibe_match_bonus
+            place.properties.vibe_score += vibe_bonus
+
+
+            // TODO: Places that match vibe get a bonus
+
+            // Give place an event score
+            // TODO: Sum of events is a stand in for a better metric of a places relevance
+            
+            place.properties.num_events = place.properties.hotspots_events.features.length
+            
+            // TODO: why is this needed for icon points
+            place.properties.id = place.id
+
+
+            if (place.properties.num_events > 0) {
+                let likes = place.properties.hotspots_events.features[0].properties.likes
+                // TODO: Whats an appropriate ceiling to rank against
+                let like_score = 0
+                if(likes) like_score = likes / 100
+
+                // Simple sum of score works for now
+                place.properties.event_score = place.properties.num_events + likes
+            } else {
+                place.properties.event_score = 0
+            }
+
+            if (place.properties.aggregate_rating >= 2) {
+
+                if (place.properties.categories == undefined || place.properties.categories.length == 0) {
+                    place.properties.categories = ["missing"]
+                    // TODO: Fix missing and duplicate places.
+                    //console.log("missing: ", place.properties.name)
+                }
+
+            } else {
+                place.properties.aggregate_rating = 2
+            }
+
+            if (place.properties.aggregate_rating > max_aggregate_score)  max_aggregate_score = place.properties.aggregate_rating
+
+            if (place.properties.vibe_score > max_vibe_score) max_vibe_score = place.properties.vibe_score
+
+            if (place.properties.event_score > max_event_score) max_event_score = place.properties.event_score
+        
+            if(center_point) {
+                let point = turf.point(place.geometry.coordinates)
+                place.properties.distance = turf.distance(center_point, point)
+
+                if (place.properties.distance > max_distance) max_distance = place.properties.distance
+            }    
+
+        })
+
+        let places_scored = places.map((place) => {
+            
+            place.properties.event_score = helpers.default.normalize(place.properties.event_score, 0, max_event_score)
+            place.properties.vibe_score = helpers.default.normalize(place.properties.vibe_score, 0, max_vibe_score)
+            place.properties.aggregate_rating = helpers.default.normalize(place.properties.aggregate_rating, 0, max_aggregate_score)
+            // Distance is inverted from max and then normalize 1-10
+            place.properties.distance = helpers.default.normalize(max_distance - place.properties.distance, 0, max_distance)
+
+            // Simple average of the different scores
+            place.properties.average_score = place.properties.event_score + place.properties.vibe_score + place.properties.aggregate_rating + place.properties.distance / 4
+
+            return place
+        })
+
+        let places_scored_and_sorted = places_scored.sort((a, b) => {
+            return b.properties.average_score - a.properties.average_score
+        })
+
+        return places_scored_and_sorted
+    },
 }
