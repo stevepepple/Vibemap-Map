89bcb5125bbdabde2d833ac8766ea10cedb6fb8b
Steve Pepple <stephen.pepple@gmail.com>
2020-04-08 12:42:10 -0700

    Move Vibe picker to its own component

---
+++ client/src/components/elements/AllVibes.js
@@ -0,0 +1,68 @@
+// @flow 
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import * as actions from '../../redux/actions'
+import isEqual from 'react-fast-compare'
+
+import { Translation } from 'react-i18next'
+import { Dropdown } from 'semantic-ui-react'
+
+class AllVibes extends Component {    
+    constructor(props) {
+        super(props)
+        this.state = {
+            vibe_options : []
+        }
+    }
+
+    componentDidUpdate(prevProps) {
+        if (!isEqual(prevProps.allVibes, this.props.allVibes)) {
+            
+            let vibe_options = this.props.allVibes.map(function (vibe) {
+                return { key: vibe, value: vibe, className: 'vibe', text: vibe }
+            })
+
+            this.setState({ vibe_options : vibe_options})
+        }        
+    }    
+
+    handleVibeChange = (event, { value }) => {
+        this.setState({ vibes: value })
+        this.props.setCurrentVibes(value)
+    }
+
+    render() {
+
+        return (
+            <div className='allVibes' style={{ position: 'absolute', margin: '1em', zIndex: '100'}}>
+                <Translation>{
+                    (t, { i18n }) => <Dropdown
+                        style={{ minWidth: '12em' }}
+                        clearable
+                        placeholder={t("Add more vibes")}
+                        multiple
+                        label="Vibe"
+                        search
+                        closeOnChange
+                        onChange={this.handleVibeChange}
+                        options={this.state.vibe_options}
+                        value={this.props.currentVibes}
+                        renderLabel={this.renderVibesLabel}
+                    />
+                }</Translation>
+            </div>
+            
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    //console.log('store to weather: ', state)
+    return {
+        allVibes: state.allVibes,
+        currentVibes: state.currentVibes,
+        topVibes: state.topVibes
+    }
+}
+
+export default connect(mapStateToProps, actions)(AllVibes)
