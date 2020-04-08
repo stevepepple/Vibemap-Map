8397639eced6251ca1cd7e54d6a7189d236cc52b
Steve Pepple <stephen.pepple@gmail.com>
2020-04-08 12:59:32 -0700

    Add header component with new branding

---
+++ client/src/components/elements/header.js
@@ -0,0 +1,50 @@
+import React, { Component, Fragment } from 'react';
+import { Grid, Menu, Dropdown, Image } from 'semantic-ui-react'
+import SVG from 'react-inlinesvg'
+
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
+
+import LocationSearchInput from '../map/search'
+
+
+class Header extends Component {
+    render() {
+        return (
+            <Menu id='header' borderless pointing basic>
+                <Menu.Item>
+                    <SVG style={{ width: '12em', padding: '1em' }} src='/images/logo-type.svg' />
+                </Menu.Item>                
+
+                <Menu.Menu position='right'>
+                    <Menu.Item name='city' style={{ zIndex: '200'}}>
+                        <SVG style={{ width: '2em' }} src='/images/logo-mark.svg' />
+                        <LocationSearchInput className='mobile search' />
+                    </Menu.Item>
+                    
+                    <Menu.Item>
+                        <Dropdown button labeled className='icon main_menu' icon='list' text='Menu'>
+                            <Dropdown.Menu>
+                                <Dropdown.Item>
+                                    <Link to="/places">Nearby</Link>
+                                </Dropdown.Item>
+                                <Dropdown.Item>
+                                    <Link to="/calendar">Calendar</Link>
+                                </Dropdown.Item>
+                                <Dropdown.Item>
+                                    <Link to="/generator">Vibe Generator</Link>
+                                </Dropdown.Item>
+                                <Dropdown.Item>
+                                    <Link to="/profile">Claim Places</Link>
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Menu.Item>
+                </Menu.Menu>
+                
+            </Menu>
+        );
+    }
+}
+
+export default Header;
+
