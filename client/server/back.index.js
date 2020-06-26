5152f4b26f43ada63f00ff45a6347a64e34745c1
Steve Pepple <stephen.pepple@gmail.com>
2020-06-26 08:33:41 -0700

    Add server back up

---
+++ client/server/back.index.js
@@ -0,0 +1,60 @@
+import path from 'path';
+import fs from 'fs';
+
+import 'ignore-styles'
+
+import React from 'react';
+import express from 'express';
+import cors from 'cors'
+import ReactDOMServer from 'react-dom/server';
+
+import { Provider } from 'react-redux'
+import { store } from '../src/redux/store';
+
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+
+import { StaticRouter } from 'react-router'
+
+
+import App from '../src/App';
+
+const PORT = process.env.PORT || 3006;
+const app = express();
+
+app.use(cors())
+
+app.use(express.static('./build'));
+
+app.get('/*', (req, res) => {
+    const context = {};
+    const helmetContext = {};
+
+    //console.log('query params: ', req)
+
+    const app = ReactDOMServer.renderToString(
+        <StaticRouter
+            location={req.url}
+            context={context}>
+                <Provider store={store}>
+                    <HelmetProvider context={helmetContext}>
+                        <App/>
+                    </HelmetProvider>
+                </Provider>
+        </StaticRouter>);
+
+    const indexFile = path.resolve('./build/index.html');
+    fs.readFile(indexFile, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Something went wrong:', err);
+            return res.status(500).send('Oops, better luck next time!');
+        }
+
+        return res.send(
+            data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+        );
+    });
+});
+
+app.listen(PORT, () => {
+    console.log('😎 Server is listening on port: ', PORT);
+});
