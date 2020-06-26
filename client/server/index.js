d1ba22d1dc0b621ad667a7f8e6e22e6616a663d6
Steve Pepple <stephen.pepple@gmail.com>
2020-06-26 08:14:37 -0700

    Try different webpack config

--- client/server/index.js
+++ client/server/index.js
@@ -1,60 +1,22 @@
-import path from 'path';
-import fs from 'fs';
+const express = require('express')
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+const webpackHotServerMiddleware = require('webpack-hot-server-middleware')
+const serverConfig = require('../webpack/server.dev.js')
+const clientConfig = require('../webpack/client.dev.js')
 
-import 'ignore-styles'
+const app = express()
 
-import React from 'react';
-import express from 'express';
-import cors from 'cors'
-import ReactDOMServer from 'react-dom/server';
+const compiler = webpack([clientConfig, serverConfig])
+const clientCompiler = compiler.compilers[0]
+const publicPath = clientConfig.output.publicPath
+const options = { publicPath, stats: { colors: true } }
 
-import { Provider } from 'react-redux'
-import { store } from '../src/redux/store';
+app.use(webpackDevMiddleware(compiler, options))
+app.use(webpackHotMiddleware(clientCompiler))
+app.use(webpackHotServerMiddleware(compiler))
 
-import { Helmet, HelmetProvider } from 'react-helmet-async';
-
-import { StaticRouter } from 'react-router'
-
-
-import App from '../src/App';
-
-const PORT = process.env.PORT || 3006;
-const app = express();
-
-app.use(cors())
-
-app.use(express.static('./build'));
-
-app.get('/*', (req, res) => {
-    const context = {};
-    const helmetContext = {};
-
-    //console.log('query params: ', req)
-
-    const app = ReactDOMServer.renderToString(
-        <StaticRouter
-            location={req.url}
-            context={context}>
-                <Provider store={store}>
-                    <HelmetProvider context={helmetContext}>
-                        <App/>
-                    </HelmetProvider>
-                </Provider>
-        </StaticRouter>);
-
-    const indexFile = path.resolve('./build/index.html');
-    fs.readFile(indexFile, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Something went wrong:', err);
-            return res.status(500).send('Oops, better luck next time!');
-        }
-
-        return res.send(
-            data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
-        );
-    });
-});
-
-app.listen(PORT, () => {
-    console.log('😎 Server is listening on port: ', PORT);
-});
+app.listen(3000, () => {
+    console.log('Listening @ http://localhost:3000')
+})
---
+++ client/server/render.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import ReactDOM from 'react-dom/server'
+import { flushChunkNames } from 'react-universal-component/server'
+import { Provider } from 'react-redux'
+import flushChunks from 'webpack-flush-chunks'
+import App from '../src/App'
+
+import { store } from '../src/redux/store';
+
+export default ({ clientStats }) => (req, res) => {
+    const initialState = JSON.stringify(store.getState());
+    const app = ReactDOM.renderToString(
+        <Provider store={store}>
+            <App />
+        </Provider>
+    )
+    const chunkNames = flushChunkNames()
+
+    const {
+        js,
+        styles,
+        cssHash,
+        scripts,
+        stylesheets
+    } = flushChunks(clientStats, { chunkNames })
+
+    console.log('PATH', req.path)
+    console.log('DYNAMIC CHUNK NAMES RENDERED', chunkNames)
+    console.log('SCRIPTS SERVED', scripts)
+    console.log('STYLESHEETS SERVED', stylesheets)
+
+    res.send(
+        `<!doctype html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <title>react universal from scratch</title>
+          ${styles}
+        </head>
+        <body>
+          <script>
+            window.__INITIAL_STATE__ = ${initialState}
+          </script>
+          <div id="root">${app}</div>
+          ${cssHash}
+          ${js}
+        </body>
+      </html>`
+    )
+}
