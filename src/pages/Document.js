eec59a0e18de469249af6e289b6ef51321610d9c
Steve Pepple <stephen.pepple@gmail.com>
2020-07-08 08:00:34 -0700

    Hide debug statement

--- src/pages/Document.js
+++ src/pages/Document.js
@@ -14,6 +14,7 @@ import { Provider } from 'react-redux';
 
 class Document extends React.Component {
     static async getInitialProps({ renderPage, store }) {
+        //console.log('store on load: ', store.getState())
         const page = await renderPage(App => props => (
             <Provider store={store}>
                 <App {...props} />
