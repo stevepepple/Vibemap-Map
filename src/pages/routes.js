75065e2e689465e0302a7b1855f12f5292acedba
Steve Pepple <stephen.pepple@gmail.com>
2020-07-09 07:45:29 -0700

    Drop starter files

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -10,34 +10,30 @@ import Details from "./Details";
 import { asyncComponent } from '@jaredpalmer/after';
 
 const routes = [
-  {
-    path: "/cities/:city?",
-    exact: true,
-    component: asyncComponent({
-      loader: () => import('./Main'), // required
-      Placeholder: () => <div>...LOADING...</div>
-    })
-  },
-  {
-    path: "/map",
-    component: asyncComponent({
-      loader: () => import('./MapPage'), // required
-      Placeholder: () => <div>...LOADING...</div>
-    })
-  },
-  {
-    path: "/details/:id",
-    exact: true,
-    component: asyncComponent({
-      loader: () => import('./Details'), // required
-      Placeholder: () => <div>...LOADING...</div>
-    }),
-    
-  },
-  {
-    path: "/news",
-    component: News
-  }
+	{
+		path: "/cities/:city?",
+		exact: true,
+		component: asyncComponent({
+			loader: () => import('./Main'), // required
+			Placeholder: () => <div>...LOADING...</div>
+		})
+	},
+	{
+		path: "/map",
+		component: asyncComponent({
+			loader: () => import('./MapPage'), // required
+			Placeholder: () => <div>...LOADING...</div>
+		})
+	},
+	{
+		path: "/details/:id",
+		exact: true,
+		component: asyncComponent({
+			loader: () => import('./Details'), // required
+			Placeholder: () => <div>...LOADING...</div>
+		}),
+
+	}
 ];
 
 export default routes;
